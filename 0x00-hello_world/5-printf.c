eaa6b32a9d92e0ca32e8f5a59731549eb5ab0eda
frederick <amfred1234@gmail.com>
2023-09-09 02:55:51 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,11 @@
 /**
  * main - this is the main function
  *
- * Return: always 0
+ * Return: 0
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	printf("with proper grammar, but the outcome is a piece of art,|");
 	return (0);
 }
