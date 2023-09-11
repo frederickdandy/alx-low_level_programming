6060b84d7367e264954264237301eed8b0679556
frederick <amfred1234@gmail.com>
2023-09-11 12:31:56 -0700

    base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return:0
+ */
+int main(void)
+{
+	int i;
+	char k;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	for (k = 'a' ; k <= 'f' ; k++)
+		putchar(k);
+	putchar('\n');
+	return (0);
+}
