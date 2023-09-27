87fc454a525fce42b49d9a2493dfc4a83eeaddbb
frederick <amfred1234@gmail.com>
2023-09-27 13:26:36 -0700

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,12 +3,12 @@
 /**
  * _puts_recursion - prints string
  * @s: string to print
- * 
+ *
  */
 
 void _puts_recursion(char *s)
 {
-	if(*s != '\0')
+	if (*s != '\0')
 	{
 		putchar(*s);
 		_puts_recursion(s + 1);
